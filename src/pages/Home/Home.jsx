b390bb4087efc340d06e7b2bb83e80ab9801d2a0
LuizFelixDev <luizhenriquefelix138@gmail.com>
2026-07-25 15:11:50 -0300

    feat: Adicionando moldura e foto de perfil

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import './styles.css'
+import foto from '../../assets/image.png'
+import bolinha from '../../assets/bolinha.png'
 
 export function Home() {
 
@@ -7,13 +9,13 @@ export function Home() {
       <header>
         <p className='textroboticbar'>DEV.PORTIFOLIO</p>
       </header>
-      <div>
-        <div>
+      <div className='container'>
+        <div className='information'>
           <p className='textrobotic'>&gt;  HELLO_WORLD.EXE</p>
           <h1 className='title'>
             Luiz Henrique <br />
             <span className='azul-claro'>
-              Software Developer
+              Software Developer.
             </span>
           </h1>
           <p className='text-home'>
@@ -22,9 +24,20 @@ export function Home() {
             complexas em código limpo e experiências que importam.
           </p>
         </div>
+        <div className='moldura'>
+          <div className='foto'>
+            <img src={foto} alt="" className='img' />
+            <div className='opentowork'>
+              <img src={bolinha} className='bolinha' />
+              <p className='text'> aberto para trabalhos</p>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     </div>
+
   )
 }
 
