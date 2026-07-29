da4112fd57c9eb9d6edd18610e11228735ca2b42
LuizFelixDev <luizhenriquefelix138@gmail.com>
2026-07-29 10:27:25 -0300

    feat: Adicionando cards vazios de formação e experiências

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,31 +1,43 @@
 import './styles.css';
 
-export function Sobre(){
-  return(
-    <div className='body-sobre'>
-      <p className='textrobotichsobre'>01 - ABOUT</p>
-      <h1 className='titlesobre'>Sobre mim</h1>
-      <div className='sobrea'>
-        <p>Sou estudante de Sistemas de Informação na UFRN e tenho <br />
-        grande interesse em desenvolvimento frontend e mobile. <br />
-        Tenho experiência com React Native, React, Next.js, <br />
-        JavaScript, Node.js e TypeScript, criando interfaces <br />
-        modernas, responsivas e focadas na melhor experiência para <br />
-        o usuário.
-        </p>
-        <p className='sobreb'>
-            Iniciei minha trajetória no Instituto Federal da Paraíba, onde me <br />
-            formei técnico, e hoje curso SI na UFRN buscando sempre <br />
-            aprimorar minhas habilidades. Trabalho com integração de APIs, <br />
-            gerenciamento de estado, navegação entre telas e Git/GitHub no <br />
-            controle de versão.
-        </p>
-        <p className='sobreb'>
-            Atualmente atuo como desenvolvedor back-end, com foco em soluções <br />
-            eficientes e escaláveis. Mantenho projetos open source no GitHub e estou <br />
-            sempre em busca de desafios de alto impacto.
-        </p>
-      </div>
-    </div>
-  )
+export function Sobre() {
+    return (
+        <div className='body-sobre'>
+            <p className='textrobotichsobre'>01 - ABOUT</p>
+            <h1 className='titlesobre'>Sobre mim</h1>
+            <hr className='linha' />
+            <div className='container-sobre'>
+                <div>
+                    <p className='sobrea'>Sou estudante de Sistemas de Informação na UFRN e tenho <br />
+                        grande interesse em desenvolvimento frontend e mobile. <br />
+                        Tenho experiência com React Native, React, Next.js, <br />
+                        JavaScript, Node.js e TypeScript, criando interfaces <br />
+                        modernas, responsivas e focadas na melhor experiência para <br />
+                        o usuário.
+                    </p>
+                    <p className='sobreb'>
+                        Iniciei minha trajetória no Instituto Federal da Paraíba, onde me <br />
+                        formei técnico, e hoje curso SI na UFRN buscando sempre <br />
+                        aprimorar minhas habilidades. Trabalho com integração de APIs, <br />
+                        gerenciamento de estado, navegação entre telas e Git/GitHub no <br />
+                        controle de versão.
+                    </p>
+                    <p className='sobreb'>
+                        Atualmente atuo como desenvolvedor back-end, com foco em soluções <br />
+                        eficientes e escaláveis. Mantenho projetos open source no GitHub e estou <br />
+                        sempre em busca de desafios de alto impacto.
+                    </p>
+                </div>
+                <div className='container-formações'>
+                    <p className='text-sobre-card'>FOMAÇÃO</p>
+                    <div className='card-sobre'></div>
+                    <div className='card-sobre'></div>
+                    <p className='text-sobre-card'>EXPERIÊNCIA</p>
+                    <div className='card-sobre'></div>
+                    <div className='card-sobre'></div>
+                    <div className='card-sobre'></div>
+                </div>
+            </div>
+        </div>
+    )
 }
